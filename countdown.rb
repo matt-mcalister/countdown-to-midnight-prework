a2928e3b6969d3cba54739bb701979efc37a7fbc
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-26 19:41:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(num)
   while counter > 0
     puts "#{{counter} SECOND(S)!}"
     counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  counter = num
+  while counter > 0
+    puts "#{{counter} SECOND(S)!}"
+    counter -= 1
     sleep(1)
   end
   puts "HAPPY NEW YEAR!"
